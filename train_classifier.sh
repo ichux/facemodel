b85d7d0cfe68db4dbbafc6ad7cc938a37f44a7d9
Chukwudi Nwachukwu <chukwudi.nwachukwu@ng.airtel.com>
2023-07-24 13:11:49 +0100

    removed some unwanted bits

--- train_classifier.sh
+++ train_classifier.sh
@@ -4,10 +4,6 @@ export PYTHONPATH=$PWD/facenet/src
 
 mkdir -p models
 
-# python facenet/src/classifier.py TRAIN $PWD/images/lfw/lfw_mtcnnpy_160 \
-# $PWD/models/model-$(date +%Y%m%dT%H%M%S-%s-%Z).pb $PWD/models/lfw_classifier.pkl --batch_size 1000 \
-# --min_nrof_images_per_class 40 --nrof_train_images_per_class 35 --use_split_dataset
-
 python facenet/src/classifier.py TRAIN $PWD/images/lfw/lfw_mtcnnpy_160 \
   $PWD/pre-trained-models/20180408-102900/20180408-102900.pb \
   $PWD/pre-trained-models/20180408-102900/lfw_classifier.pkl --batch_size 1000 \
