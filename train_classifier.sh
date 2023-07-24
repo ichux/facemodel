47180949237fe8f4627f5eca0f4a0e0c0520930d
Chukwudi Nwachukwu <chukwudi.nwachukwu@ng.airtel.com>
2023-07-24 13:06:05 +0100

    improved on code

--- train_classifier.sh
+++ train_classifier.sh
@@ -9,6 +9,7 @@ mkdir -p models
 # --min_nrof_images_per_class 40 --nrof_train_images_per_class 35 --use_split_dataset
 
 python facenet/src/classifier.py TRAIN $PWD/images/lfw/lfw_mtcnnpy_160 \
-$PWD/models/20180402-114759.pb $PWD/models/lfw_classifier.pkl --batch_size 1000 \
+  $PWD/pre-trained-models/20180408-102900/20180408-102900.pb \
+  $PWD/pre-trained-models/20180408-102900/lfw_classifier.pkl --batch_size 1000 \
 --min_nrof_images_per_class 40 --nrof_train_images_per_class 35 --use_split_dataset
 
